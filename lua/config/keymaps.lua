d38828d98076102b5f38091911d8162f4b3fde47
fedorevgr <lpacwms3000@mail.ru>
2024-11-12 22:53:46 +0300

    c support, 4 space tab

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Add any additional keymaps heex
+--
+
+-- local keymap = vim.keymap
+
+-- keymap.set("n", "<S->b", '<cmd>bnext<cr>', {})
--- lua/plugins/clang_goodies.lua
+++ lua/plugins/clang_goodies.lua
@@ -1,32 +1,35 @@
 return {
-  "p00f/clangd_extensions.nvim",
-  ft = { "c", "cpp", "h" },
-  dependencies = {
-    "VonHeikemen/lsp-zero.nvim",
-    "neovim/nvim-lspconfig",
-    "nvim-lua/plenary.nvim",
-  },
-  keys = {
-    { "<A-h>", "<CMD>ClangdSwitchSourceHeader<CR>", mode = { "n", "i", "v" } }, -- TODO header switch
-  },
-  config = function()
-    local server_options = require("lsp-zero").build_options("clangd", {
-      settings = {
-        clangd = {
-          InlayHints = {
-            Designators = true,
-            Enabled = true,
-            ParameterNames = true,
-            DeducedTypes = true,
-          },
-        },
-        fallbackFlags = { "-std=c23" },
-      },
-      cmd = {
-        "clangd",
-      },
-    })
-    require("lspconfig").clangd.setup(server_options)
-    require("clangd_extensions").setup()
-  end,
+	"p00f/clangd_extensions.nvim",
+	ft = { "c", "cpp", "h" },
+	dependencies = {
+		"VonHeikemen/lsp-zero.nvim",
+		"neovim/nvim-lspconfig",
+		"nvim-lua/plenary.nvim",
+	},
+	keys = {
+		{ "<A-h>", "<CMD>ClangdSwitchSourceHeader<CR>", mode = { "n", "i", "v" } }, -- TODO header switch
+	},
+	config = function()
+		local server_options = require("lsp-zero").build_options("clangd", {
+			settings = {
+				clangd = {
+					InlayHints = {
+						Designators = true,
+						Enabled = true,
+						ParameterNames = true,
+						DeducedTypes = true,
+					},
+				},
+				fallbackFlags = { "-std=c23" },
+			},
+			cmd = {
+				"clangd",
+			},
+		})
+		require("lspconfig").clangd.setup(server_options)
+		require("clangd_extensions").setup()
+	end,
+	build_options = {
+		"-std=c99",
+	},
 }
